8ace152899f1a766034013c2b56132474c239e50
jeff <xorgus1234@gmail.com>
2022-01-06 14:24:03 +0900

    Feat: PhotoCell xib 구현

--- Splash/Splash/Presentation/PhotoCell.swift
+++ Splash/Splash/Presentation/PhotoCell.swift
@@ -1,20 +1,21 @@
 //
-//  SquarePhotoCell.swift
+//  PhotoCell.swift
 //  Splash
 //
-//  Created by TakHyun Jung on 2022/01/04.
+//  Created by TakHyun Jung on 2022/01/05.
 //
 
 import UIKit
 
-class SquarePhotoCell: UICollectionViewCell {
-    static let identifider: String = String(describing: SquarePhotoCell.self)
-    
+class PhotoCell: UICollectionViewCell {
+    static let identifider: String = String(describing: PhotoCell.self)
+
     @IBOutlet weak var imageView: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         self.imageView.layer.cornerRadius = 10
         self.imageView.contentMode = .scaleAspectFill
+        self.imageView.clipsToBounds = true
     }
 }
