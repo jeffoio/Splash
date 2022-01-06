b99644b951096d067eaf254b03e1dfd307b70927
jeff <xorgus1234@gmail.com>
2022-01-06 23:52:32 +0900

    Feat: Home 화면 로직 수정

--- Splash/Splash/Presentation/HomeViewController.swift
+++ Splash/Splash/Presentation/HomeViewController.swift
@@ -13,61 +13,52 @@ class HomeViewController: UIViewController {
         case photos
     }
     
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var topicCollectionView: UICollectionView!
-    @IBOutlet weak var photoCollectionView: UICollectionView!
-    @IBOutlet weak var backButton: UIButton!
-    @IBOutlet weak var photoCollectionViewTopConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var topicCollectionView: UICollectionView!
+    @IBOutlet private weak var photoCollectionView: UICollectionView!
     
-    var viewModel: HomeViewModel!
-    var topicDataSource: UICollectionViewDiffableDataSource<Section, Topic>?
-    var photoDataSource: UICollectionViewDiffableDataSource<Section, PhotoInformation>?
+    private var viewModel: HomeViewModel!
+    private var topicDataSource: UICollectionViewDiffableDataSource<Section, Topic>?
+    private var photoDataSource: UICollectionViewDiffableDataSource<Section, PhotoInformation>?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.inject()
         self.configure()
-        viewModel.fetch()
+        self.viewModel.fetch()
     }
     
     func inject() {
         let service = TransferService(networkService: NetworkService())
-        let repo = PhotoInformationRepository(service: service)
-        let usecase = PhotoInformationUsecase(repository: repo)
+        let repo = TopicPhotoInformationRepository(service: service)
+        let usecase = TopicPhotoInformationUsecase(repository: repo)
         self.viewModel = HomeViewModel(usecase: usecase)
     }
     
     @objc func topicButtonTapped(_ sender: UIButton) {
-        guard let string = sender.currentTitle,
-              let topic = Topic(rawValue: string) else { return }
+        guard let string = sender.currentTitle, let topic = Topic(rawValue: string) else { return }
         self.viewModel.update(topic)
     }
     
-    @IBAction func backButtonTapped(_ sender: UIButton) {
-        self.photoCollectionView.setCollectionViewLayout(self.generateVertialLayout(), animated: false) { [weak self] _ in
-            DispatchQueue.main.async {
-                self?.updatePhotoCollectionViewConstraint(false)
-                self?.backButton.isHidden = true
-                self?.titleLabel.text = "Splash"
-            }
+    func showDetail(indexPath: IndexPath) {
+        guard let detailViewController = self.storyboard?.instantiateViewController(identifier: DetailViewController.identifider, creator: { coder in
+            return DetailViewController(coder: coder, photos: self.viewModel.photos.value, indexPath: indexPath)
+        }) else {
+            fatalError("Failed to load DetailViewController from storyboard.")
         }
+        detailViewController.delegate = self
+        self.present(detailViewController, animated: false, completion: nil)
     }
-    
-    func updatePhotoCollectionViewConstraint(_ isWider: Bool) {
-        let constanst = isWider ? 23.0 : 76.0
-        self.topicCollectionView.isHidden = isWider
-        self.photoCollectionViewTopConstraint.constant = constanst
-        self.photoCollectionView.setNeedsLayout()
+}
+
+extension HomeViewController: PhotoDelegate {
+    func updateCurrent(_ indexPath: IndexPath) {
+        self.photoCollectionView.scrollToItem(at: indexPath, at: .centeredVertically, animated: true)
     }
 }
 
 extension HomeViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        guard let photo = self.photoDataSource?.itemIdentifier(for: indexPath) else { return }
-        self.updatePhotoCollectionViewConstraint(true)
-        self.photoCollectionView.setCollectionViewLayout(self.generateHorizontalLayout(), animated: false)
-        self.backButton.isHidden = false
-        self.titleLabel.text = photo.user.name
+        self.showDetail(indexPath: indexPath)
     }
     
     func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
@@ -78,12 +69,21 @@ extension HomeViewController: UICollectionViewDelegate {
 
 private extension HomeViewController {
     func configure() {
-        self.photoCollectionView.delegate = self
+        self.registerNib()
+        self.conifigureDelegates()
         self.configureTopicCollectionViewLayout()
         self.configurePhotoCollectionViewLayout()
         self.configureTopicDataSource()
         self.configurePhotoDataSource()
         self.bindViewModel()
+    }
+    
+    func registerNib() {
+        photoCollectionView.register(UINib(nibName: PhotoCell.identifider, bundle: nil), forCellWithReuseIdentifier: PhotoCell.identifider)
+    }
+    
+    func conifigureDelegates() {
+        self.photoCollectionView.delegate = self
     }
     
     func configureTopicCollectionViewLayout() {
@@ -103,24 +103,15 @@ private extension HomeViewController {
         let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1)))
         let group = NSCollectionLayoutGroup.vertical(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(1)), subitem: item, count: 1)
         let section = NSCollectionLayoutSection(group: group)
-        section.interGroupSpacing = 15
-        return UICollectionViewCompositionalLayout(section: section)
-    }
-    
-    func generateHorizontalLayout() -> UICollectionViewCompositionalLayout {
-        let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1)))
-        let group = NSCollectionLayoutGroup.horizontal(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1)), subitem: item, count: 1)
-        let section = NSCollectionLayoutSection(group: group)
-        section.orthogonalScrollingBehavior = .groupPaging
-        section.interGroupSpacing = 15
+        section.interGroupSpacing = 10
         return UICollectionViewCompositionalLayout(section: section)
     }
     
     func configureTopicDataSource() {
         self.topicDataSource = UICollectionViewDiffableDataSource<Section, Topic>(collectionView: self.topicCollectionView, cellProvider: { collectionView, indexPath, topic in
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TopicCell.identifider, for: indexPath) as? TopicCell
-            cell?.button.setTitle(topic.string, for: .normal)
-            cell?.button.addTarget(self, action: #selector(self.topicButtonTapped), for: .touchUpInside)
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TopicCell.identifider, for: indexPath) as? TopicCell else { return TopicCell() }
+            cell.button.setTitle(topic.string, for: .normal)
+            cell.button.addTarget(self, action: #selector(self.topicButtonTapped), for: .touchUpInside)
             return cell
         })
         
@@ -132,9 +123,8 @@ private extension HomeViewController {
     
     func configurePhotoDataSource() {
         self.photoDataSource = UICollectionViewDiffableDataSource<Section, PhotoInformation>(collectionView: self.photoCollectionView, cellProvider: { collectionView, indexPath, photo in
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SquarePhotoCell.identifider, for: indexPath) as? SquarePhotoCell
-            let data = try! Data(contentsOf: URL(string: photo.urls.small)!)
-            cell?.imageView.image = UIImage(data: data)
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoCell.identifider, for: indexPath) as? PhotoCell else { return PhotoCell() }
+            cell.imageView.setImageUrl(id: photo.id, url: URL(string: photo.urls.small)!)
             return cell
         })
     }
